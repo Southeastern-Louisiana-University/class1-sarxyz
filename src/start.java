b74865f55c773488619c9fdee9b54e526a8548b3
userSelu <sar@selu.edu>
2025-01-23 12:16:23 -0600

    first commit

--- src/start.java
+++ src/start.java
@@ -1,7 +1,14 @@
 package src;
 
 public class start {
-    public static void main(String args[]){
-
+    public static void main(String args[]) {
+        double score = 0;
+        if (score >= 60.0){
+            System.out.println("D");
+        System.out.println("Try Harder!");
+        }
+        else if (score >= 70) {
+            System.out.println("C");
+        }
     }
 }
---
+++ src1/reviewPart1.java
@@ -0,0 +1,7 @@
+package src1;
+
+public class reviewPart1 {
+    public static void main (String [] args){
+        System.out.println("Hello Java");
+    }
+}
