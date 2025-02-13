9e229172e0ab08360afa6e8a900c83f08db08fb7
Steele Russell <sarxyz@users.noreply.github.com>
2025-02-13 12:05:24 -0600

    first commit

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,27 @@
 package src;
 
 public class Car {
+    // Attributes
+    public String make;
+    public String model;
+    private int year;
+    // CONSTRUCTOR
+    public Car(String carMake, String carModel, int carYear){
+        make = carMake;
+        model = carModel;
+        year = carYear;
+    }
+
+    public Car(){
+
+    }
+    public static void main(String[] args){
+        Car car = new Car();
+        Car carEthan = new Car("Hyundai", "VeraCruz", 2011);
+        carEthan.toString();
+
+    }
+    //public String toString(){
+    //    return toString() + " new stuff\n";
+    //};
 }
